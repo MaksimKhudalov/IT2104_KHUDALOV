a9e44bf4389669fcd60e7aab84ace896947cdb38
MaksimKhudalov <mkhudalov@mail.ru>
2024-03-23 16:01:13 +0300

    Добавлены кнопки с своими функциями, изменено название приложения, изменены цвета кнопок

--- laba3/lib/main.dart
+++ laba3/lib/main.dart
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Инкремент',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Инкремент'),
     );
   }
 }
@@ -64,12 +64,26 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Значение инкремента',
             ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            Row(              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  onPressed: _decrementCounter,
+                  child: Icon(Icons.remove),
+                  backgroundColor: Colors.red,),
+                FloatingActionButton(
+                  onPressed: _incrementCounter,
+                  child: Icon(Icons.add),
+                  backgroundColor: Colors.green,)
+              ],
+            ),
+            TextButton(onPressed: zero, 
+                child: Text("Сбросить", style: TextStyle(color: Colors.grey)))
           ],
         ),
       ),
